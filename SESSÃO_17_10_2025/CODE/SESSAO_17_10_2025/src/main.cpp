925f697de2dfa3c843bb1143e7e7026aa4e8c3b3
Carvalhinho11 <tomas13carvalho@gmail.com>
2025-10-17 12:35:22 +0100

    Add files via upload

--- SESSÃO_17_10_2025/CODE/SESSAO_17_10_2025/src/main.cpp
+++ SESSÃO_17_10_2025/CODE/SESSAO_17_10_2025/src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 
+int botao = 0;
+
 void setup() 
 {
   Serial.begin(9600);
   
+  pinMode(4,INPUT);
   pinMode(2, OUTPUT);
+
 }
 
-void loop()
+void loop() 
 {
- 
-  digitalWrite(2, HIGH);
-  delay(500);
+  botao = digitalRead(4);
 
-  digitalWrite(2, LOW);
+  if(botao)
+  {
+    digitalWrite (2, HIGH);
+    Serial.println("Botao nao pressionado");
+  }
+  else
+  {
+    digitalWrite (2, LOW);
+    Serial.println("Botao pressionado");     
+  }
   delay(500);
 }
