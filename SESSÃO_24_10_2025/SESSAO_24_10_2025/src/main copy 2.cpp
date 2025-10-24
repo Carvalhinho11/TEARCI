8c52c40c8885baa8a41506ab24fc896e4b271857
Carvalhinho11 <tomas13carvalho@gmail.com>
2025-10-24 16:45:28 +0100

    Add files via upload

---
+++ SESSÃO_24_10_2025/SESSAO_24_10_2025/src/main copy 2.cpp
@@ -0,0 +1,33 @@
+// #include <Arduino.h>
+// unsigned long t = 0;
+// unsigned long t2 = 0;
+// unsigned long tempo_anterior = 0;
+// unsigned long tempo_anterior2 = 0;
+
+// void setup() {
+// pinMode(LED_BUILTIN, OUTPUT);
+// Serial.begin(9600);
+// }
+
+// void loop() {
+// if (t - tempo_anterior > 2000)
+// {
+// digitalWrite(LED_BUILTIN, HIGH);
+// Serial.print("t -");
+// Serial.println(t);
+// Serial.print("tempo_anterior - ");
+// Serial.println(tempo_anterior);
+// tempo_anterior = millis();
+// }
+// if (t2 - tempo_anterior2 > 1000)
+// {
+// digitalWrite(LED_BUILTIN, HIGH);
+// Serial.print("t2 -");
+// Serial.println(t2);
+// Serial.print("tempo_anterior2 - ");
+// Serial.println(tempo_anterior2);
+// tempo_anterior2 = millis();
+// }
+// }
+
+
